56df91461b89aabb1cac07d33d306729de4fbeb2
ZAIN UL ABIDIN <108021130+zainansar41@users.noreply.github.com>
2023-05-06 11:33:55 +0500

    abc

--- Hooks/AuthenticationHools.js
+++ Hooks/AuthenticationHools.js
@@ -1,45 +1,52 @@
-import { getAuth, signInWithPopup, GoogleAuthProvider, signInWithEmailAndPassword } from "firebase/auth";
+import { getAuth, signInWithPopup, GoogleAuthProvider, signInWithEmailAndPassword, signInWithRedirect } from "firebase/auth";
 
 import { auth, provider } from '../firebase'
 import { app } from "../firebase";
 
 export async function googleSignin() {
-    console.log("googleSignin function called");
-  
-    try {
-      const auth = getAuth(app);
-      const provider = new GoogleAuthProvider();
-  
-      const result = await signInWithPopup(auth, provider);
-      console.log("error");
+  console.log("googleSignin function called");
 
-      const user = result.user;
-  
-      console.log("Google sign-in successful:", user);
-      return user.uid;
-    } catch (error) {
-      console.log("Google sign-in error:", error);
-      return null;
-    }
+  try {
+    const auth = getAuth(app);
+    const provider = new GoogleAuthProvider();
+
+    signInWithRedirect(auth, provider);
+
+    // After redirect, handle sign-in result
+    getAuth()
+      .getRedirectResult()
+      .then((result) => {
+        // Handle successful sign-in
+        console.log("Sign-in successful:", result.user);
+      })
+      .catch((error) => {
+        // Handle sign-in error
+        console.error("Sign-in error:", error);
+      });
+    return user.uid;
+  } catch (error) {
+    console.log("Google sign-in error:", error);
+    return null;
   }
+}
 
-  
-  
-  
-  
-  
-  
+
+
+
+
+
+
 
 export async function loginEmail(email, password) {
-    console.log("in email login");
-    console.log(email, password);
-    try {
-        const userCredential = await signInWithEmailAndPassword(auth, email, password);
-        const user = userCredential.user.uid;
-        console.log("Email sign-in successful:", user);
-        return user;
-    } catch (error) {
-        console.log("Email sign-in error:", error);
-        return null;
-    }
+  console.log("in email login");
+  console.log(email, password);
+  try {
+    const userCredential = await signInWithEmailAndPassword(auth, email, password);
+    const user = userCredential.user.uid;
+    console.log("Email sign-in successful:", user);
+    return user;
+  } catch (error) {
+    console.log("Email sign-in error:", error);
+    return null;
+  }
 }
